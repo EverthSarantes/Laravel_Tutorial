d1e0848e57357da503a8a9fb4281c0424a5f5c90
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-05-30 14:06:55 -0600

    refactor: creo vista personalizada para el welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Bienvenido a Laravel Tutorial</div>
+
+                    <div class="card-body">
+                        <p>En este proyecto, aprenderás paso a paso cómo realizar un CRUD básico en Laravel.</p>
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <a href="#" title="Aqui irás directamente al resultado del Proyecto para que lo Pruebes"
+                                    class="btn btn-primary">
+                                    <i class="fas fa-rocket"></i> Ir al Proyecto
+                                </a>
+                            </li>
+                            <li class="list-group-item">
+                                <a href="https://github.com/StevenU21" target="_blank"
+                                    title="En el Repositorio estará el Paso a Paso de la Creación del Proyecto"
+                                    class="btn btn-secondary">
+                                    <i class="fab fa-github"></i> Ir al Repositorio del Creador
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
